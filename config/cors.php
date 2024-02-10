e917e8d2f0c7562bf7f6bc2444e006191f4c97bb
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-02-10 20:41:36 +0500

    л

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://95.142.94.22:1909'],
 
     'allowed_origins_patterns' => [],
 
