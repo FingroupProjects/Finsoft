ad98b5526804e113f7fd576a4260caf30e90c222
FTursunboy <tfaiziev04@gmail.com>
2024-02-21 09:07:28 +0500

    employee and position pagination

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Position extends Model
 {
-    use HasFactory;
+    use Searchable;
 
-    protected $guarded = false;
+    protected $fillable = ['name'];
+
+    public function toSearchableArray(): array
+    {
+        return [
+            'name' => $this->name
+        ];
+    }
 }
