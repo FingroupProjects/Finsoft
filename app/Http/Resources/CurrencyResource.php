cb7f30dec2600fe3d9e673fbd763b026d6234f0d
FTursunboy <tfaiziev04@gmail.com>
2024-03-06 16:33:23 +0500

    bz

--- app/Http/Resources/CurrencyResource.php
+++ app/Http/Resources/CurrencyResource.php
@@ -19,15 +19,11 @@ class CurrencyResource extends JsonResource
             'name' => $this->name,
             'digital_code' => $this->digital_code,
             'symbol_code' => $this->symbol_code,
-<<<<<<< HEAD
             'last_exchange_rate' => ExchangeRateResource::make(
                 $this->whenLoaded('exchangeRates', function ($exchangeRates) {
                     return $exchangeRates->sortByDesc('date')->first();
                 })
             ),
-=======
-            'last_exchange_rate' =>  ExchangeRateResource::make($this->exchangeRates()->latest('date')->first()),
->>>>>>> 0023e10 (fix bug)
             'exchangeRates' => ExchangeRateResource::collection($this->whenLoaded('exchangeRates')),
             'deleted_at' => $this->deleted_at,
         ];
