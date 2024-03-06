303ff3caf93a29af5dd511bec13cb0f30bdd9316
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-03-06 10:36:16 +0500

    change contract_number

--- app/Http/Requests/Api/CounterpartyAgreement/CounterpartyAgreementRequest.php
+++ app/Http/Requests/Api/CounterpartyAgreement/CounterpartyAgreementRequest.php
@@ -24,7 +24,6 @@ class CounterpartyAgreementRequest extends FormRequest
     {
         return [
             'name' => ['string', 'required'],
-            'contract_number' => ['required', 'string'],
             'date' => ['required'],
             'organization_id' => ['required', Rule::exists('organizations', 'id')],
             'counterparty_id' => ['required', Rule::exists('counterparties', 'id')],
