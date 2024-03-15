d6b182c5168eec89146aa16a2b6140f9ad4869c0
Tursunboy Faiziev <107097209+FTursunboy@users.noreply.github.com>
2024-03-15 17:02:40 +0500

    Update Group.php

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -5,15 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Group extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['name'];
 
 
     public function employees(): HasMany
     {
         return $this->hasMany(Employee::class);
+    }
+      public static function bootSoftDeletes()
+    {
+
     }
 
 }
