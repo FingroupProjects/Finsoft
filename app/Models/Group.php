bfcb2ba6e8884b7c4073ea7f417c22a471db9be7
FTursunboy <tfaiziev04@gmail.com>
2024-03-15 17:23:31 +0500

    changes

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'type'];
 
 
     public function users(): HasMany
