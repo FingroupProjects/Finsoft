d58fa826aff5312967e78bc65edf27c36494bb1a
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2024-03-16 15:09:52 +0500

    ssd

--- app/Http/Resources/BarcodeResource.php
+++ app/Http/Resources/BarcodeResource.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 /** @mixin \App\Models\Barcode */
+
 class BarcodeResource extends JsonResource
 {
     public function toArray(Request $request): array
